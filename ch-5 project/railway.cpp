c2f465a9410da792e8093700e616a65a05fafc57
kalpeshgoswami <kalpeshgoswami298@gmail.com>
2026-01-06 16:51:28 +0530

    add train project

--- ch-5 project/railway.cpp
+++ ch-5 project/railway.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 #include <vector>
 
 using namespace std;
@@ -8,18 +8,177 @@ class Train
 {
 private:
     int trainNumber;
-    char trainName[50];
-    char source[50];
-    char destination[50];
-    char trainTime[10];
+    string trainName;
+    string source;
+    string destination;
+    string trainTime;
 
     static int trainCount;
 
-public:     //default constructor  
+public:
+    Train()
+    {
+        trainNumber = 0;
+        trainName = "not set";
+        source = "not set";
+        destination = "not set";
+        trainTime = "not set";
+    }
+    Train(int num, string name, string source, string destination, string time)
+    {
 
-Train(){
-    trainNumber=0;
-    
-}
+        trainNumber = num;
+        trainName = name;
+        this->source = source;
+        this->destination = destination;
+        trainTime = time;
+    }
 
+    Train(const Train &t)
+    {
+
+        trainNumber = t.trainNumber;
+        trainName = t.trainName;
+        source = t.source;
+        destination = t.destination;
+        trainTime = t.trainTime;
+    }
+
+    int getTrain() const
+    {
+        return trainNumber;
+    }
+
+    void display() const
+    {
+        cout << "\ntrain namber : " << trainNumber
+             << "\ntrain name   :" << trainName
+             << "\nsource       :" << source
+             << "\ndestination  : " << destination
+             << "\ntrain time   :" << trainTime << endl;
+    }
 };
+
+class RailwaySystem
+{
+
+private:
+    vector<Train> trains;
+
+public:
+    void addTrain()
+    {
+
+        int t_number;
+        string t_name;
+        string t_source;
+        string t_destination;
+        string t_time;
+
+        cout << "\nEnter train number  : ";
+        cin >> t_number;
+        cin.ignore();
+
+        cout << "Enter train  Name : ";
+        getline(cin, t_name);
+
+        cout << "Enter train  source : ";
+        getline(cin, t_source);
+
+        cout << "Enter train  destination : ";
+        getline(cin, t_destination);
+
+        cout << "Enter train  time : ";
+        getline(cin, t_time);
+
+        Train t(t_number, t_name, t_source, t_destination, t_time);
+        trains.push_back(t);
+
+        cout << "Train added successfully\n";
+    }
+
+    void displayAllData() const
+    {
+
+        cout << "\n--- All Train Details ---\n"
+             << endl;
+
+        if (trains.empty())
+        {
+            cout << "\n no train details\n"
+                 << endl;
+            return;
+        }
+
+        else
+        {
+            for (int i = 0; i < trains.size(); i++)
+            {
+                trains[i].display();
+            }
+        }
+    }
+
+    void searchByNumber(int number) const
+    {
+        for (int i = 0; i < trains.size(); i++)
+        {
+            if (trains[i].getTrain() == number)
+            {
+                cout << "\n Train Found:\n";
+                trains[i].display();
+                return;
+            }
+        }
+        cout << "\nTrain not found :\n";
+    }
+};
+
+int main()
+{
+
+    RailwaySystem manager;
+
+    int choice;
+    do
+    {
+        cout << "\n\n --- Railway Managment System ---" << endl;
+        cout << "1. Add Train " << endl;
+        cout << "2. Display All Train " << endl;
+        cout << "3. Search Train number" << endl;
+        cout << "4. Exit" << endl;
+
+        cout << "Enter your choice : ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            manager.addTrain();
+            break;
+
+        case 2:
+            manager.displayAllData();
+            break;
+
+        case 3:
+            int number;
+            cout << "Enter number to search :";
+            cin >> number;
+            manager.searchByNumber(number);
+            break;
+
+        case 4:
+            cout << " Exiting program...\n";
+            break;
+
+        default:
+            cout << " Invalid choice. Try again.\n";
+        }
+
+    }
+
+    while (choice != 0);
+
+    return 0;
+}
